b35c687d721bfbbb5e2b4fc895a8d301e50b1efb
virashu <virashu@disroot.org>
2025-11-01 20:25:56 +0300

    feat: mpeg packet draft

---
+++ examples/download_stream.rs
@@ -0,0 +1,35 @@
+use std::{fs, io::Write};
+
+use srt::server::Server;
+use tracing::Level;
+
+fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt().with_max_level(Level::INFO).init();
+
+    let mut srt_server = Server::new()?;
+
+    srt_server.on_connect(&|conn| {
+        let id = conn.stream_id.clone().unwrap_or_default();
+        tracing::info!("Client connected: {id:?}");
+        fs::write(format!("_local/stream_{id}.mpg"), []).unwrap();
+    });
+
+    srt_server.on_disconnect(&|conn| {
+        let id = conn.stream_id.clone().unwrap_or_default();
+        tracing::info!("Client disconnected: {id:?}");
+    });
+
+    srt_server.on_data(&|conn, mpeg_packet| {
+        let id = conn.stream_id.clone().unwrap_or_default();
+
+        let mut file = fs::OpenOptions::new()
+            .append(true)
+            .open(format!("_local/stream_{id}/.ts"))
+            .unwrap();
+
+        file.write_all(mpeg_packet).unwrap();
+    });
+
+    tracing::info!("Starting SRT");
+    srt_server.run()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use srt::server::Server;
+use mpeg::packet::Packet as MpegPacket;
+use srt::server::Server as SrtServer;
 use tracing::Level;
-
-use std::{fs, io::Write, time::SystemTime};
 
 fn run_hls() -> anyhow::Result<()> {
     tracing::info!("Starting HLS");
@@ -13,7 +12,7 @@ fn run_hls() -> anyhow::Result<()> {
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
 
-    let mut srt_server = Server::new()?;
+    let mut srt_server = SrtServer::new()?;
 
     srt_server.on_connect(&|conn| {
         tracing::info!(
@@ -32,7 +31,14 @@ fn main() -> anyhow::Result<()> {
     srt_server.on_data(&|conn, mpeg_packet| {
         let id = conn.stream_id.clone().unwrap_or_default();
 
-        println!("{mpeg_packet:?}");
+        let pack = MpegPacket::from_raw(mpeg_packet).unwrap();
+
+        match pack.packet_id {
+            0x000 => println!("PAT"),
+            0x100 => println!("Video"),
+            0x101 => println!("Audio"),
+            n => println!("0x{n:X}"),
+        }
     });
 
     tracing::info!("Starting SRT");
